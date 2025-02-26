2c77e15dc3aaaa02dd486db03de0e4b443896d65
Matheus Barbosa <mamateubosa00@gmail.com>
2025-02-26 14:19:54 -0300

    refactor user registration to implement UsersRepository interface and handle user existence errors

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,5 +1,7 @@
 import { prisma } from '@/lib/prisma'
+import type { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -8,19 +10,15 @@ interface RegisterUseCaseRequest {
 }
 
 export class RegisterUseCase {
-  constructor(private usersRepository: any) {}
+  constructor(private usersRepository: UsersRepository) {}
 
   async execute({ name, email, password }: RegisterUseCaseRequest) {
     const password_hash = await hash(password, 6)
 
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error('E-mail already exists')
+      throw new UserAlreadyExistsError()
     }
 
     // const prismaUsersRepository = new PrismaUsersRepository()
