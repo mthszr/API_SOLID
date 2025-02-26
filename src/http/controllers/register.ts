1a47d4d2dda96a34d3e116dbbc8ac953fbeba7fa
Matheus Barbosa <mamateubosa00@gmail.com>
2025-02-26 12:15:51 -0300

    refactor user registration to use RegisterUseCase class and PrismaUsersRepository for improved structure and maintainability

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,5 @@
-import { prisma } from '@/lib/prisma'
-import { registerUseCase } from '@/services/register'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { RegisterUseCase } from '@/services/register'
 import { hash } from 'bcryptjs'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
@@ -14,7 +14,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    await registerUseCase({
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({
       name,
       email,
       password,
