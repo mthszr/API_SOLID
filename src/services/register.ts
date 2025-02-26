5eeb088daaf5bbf31c82186140762ac1467e441c
Matheus Barbosa <mamateubosa00@gmail.com>
2025-02-26 17:32:08 -0300

    add Vite configuration and Vitest setup for testing user registration

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/lib/prisma'
 import type { UsersRepository } from '@/repositories/users-repository'
+import type { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
@@ -9,10 +10,18 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
+interface RegisteruseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisteruseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -21,12 +30,14 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    // const prismaUsersRepository = new PrismaUsersRepository()
-
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return {
+      user,
+    }
   }
 }
