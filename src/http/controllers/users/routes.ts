479e8a5cb1e9bed37d55798215f5c11959c61fe4
Matheus Barbosa <mamateubosa00@gmail.com>
2025-03-04 09:08:19 -0300

    feat: add refresh token functionality with cookie handling and corresponding tests

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { verifyJWT } from '../../middlewares/verif-jwt'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { register } from './register'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
